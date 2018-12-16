f670d5884326024706f6a5c0d88209917ef2c098
a-shokriyan <shokriyan@gmail.com>
2018-12-16 16:03:18 -0500

    base class

--- src/test/java/utills/BaseClass.java
+++ src/test/java/utills/BaseClass.java
@@ -1,5 +1,31 @@
 package utills;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BaseClass {
+
+	public static WebDriver driver;
+
+	public static void setUp(String browser) {
+
+		if (browser.equalsIgnoreCase("chrome")) {
+			if (System.getProperty("os.name").contains("Mac")) {
+				System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver");
+			} else if (System.getProperty("os.name").contains("Windows")) {
+				System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver.exe");
+			}
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			if (System.getProperty("os.name").contains("Mac")) {
+				System.setProperty("webdriver.gecko.driver", "src/test/resources/Drivers/geckodriver.exe");
+			} else if (System.getProperty("os.name").contains("Windows")) {
+				System.setProperty("webdriver.gecko.driver", "src/test/resources/Drivers/geckodriver");
+			}
+			driver = new FirefoxDriver();
+		}
+
+	}
 
 }
