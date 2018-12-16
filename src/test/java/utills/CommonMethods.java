a07b9a47ed6a9a484fc0a3d6c0b4a44ce1006254
a-shokriyan <shokriyan@gmail.com>
2018-12-16 17:16:17 -0500

    revised CommonMethods class

--- src/test/java/utills/CommonMethods.java
+++ src/test/java/utills/CommonMethods.java
@@ -4,43 +4,49 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonMethods extends BaseClass{
 	
 	
 	
-	public void enterValues (WebElement element, String value) {
+	public static void enterValues (WebElement element, String value) {
 		element.clear();
 		element.sendKeys(value);
 		
 	}
 	
-	public void click(WebElement element) {
+	public static void click(WebElement element) {
 		WebElement elem=waiting(element);
 		elem.click();
 	}
 	
-	public WebElement waiting (WebElement element) {  /// waiting for my team to finish base class
+	public static WebElement waiting (WebElement element) {  /// waiting for my team to finish base class
 		WebDriverWait wait= new WebDriverWait(driver, 30);
 		return wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
 	
-	public void jScriptExecutorClick (WebElement element) {
+	public static void jScriptExecutorClick (WebElement element) {
 		JavascriptExecutor click= (JavascriptExecutor) driver;
 		click.executeScript("argument[0].click()",element);
 	}
 	
-	public void jScriptExecutorScroll(WebElement element) {
+	public static void jScriptExecutorScroll(WebElement element) {
 		JavascriptExecutor scroll = (JavascriptExecutor) driver;
 		scroll.executeAsyncScript("argument[0].scrollIntoView(True)", element);
 	}
 	
-	public void clickonLinks(String linkText) {
+	public static void clickonLinks(String linkText) {
 		WebElement elm = driver.findElement(By.linkText(linkText));
 		WebElement linkElement = waiting(elm);
 		linkElement.click();
 		
+	}
+	
+	public static void selectDropdown (WebElement element, String value) {
+		Select dropdown= new Select(element);
+		dropdown.selectByVisibleText(value);
 	}
 
 }
