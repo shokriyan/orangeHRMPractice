16751cac4e449a17ce4c15ad61ef7392da2860a0
sayedabbasmosawi <samosawi7@gmail.com>
2018-12-16 18:34:37 -0500

    adding by force :(

--- src/test/java/stepDefinition/LoginSteps.java
+++ src/test/java/stepDefinition/LoginSteps.java
@@ -1,5 +1,39 @@
 package stepDefinition;
 
-public class LoginSteps {
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import pages.LoginPage;
+import utills.BaseClass;
+import utills.CommonMethods;
+
+public class LoginSteps extends BaseClass {
+
+	@Given("^user enter \"([^\"]*)\" and \"([^\"]*)\"$")
+	public void user_enter_and(String uName, String pword) throws Throwable {
+		
+		LoginPage log = new LoginPage();
+		log.userN(uName);
+		log.pass(pword);
+		log.loginButton();
+
+	}
+
+	@Then("^user click on Login Buton$")
+	public void user_click_on_Login_Button() throws Throwable {
+		Thread.sleep(3000);
+		LoginPage log = new LoginPage();
+		log.loginButton();
+
+	}
+
+	@Then("^user successfully Logg in$")
+	public void user_successfully_Logg_in() throws Throwable {
+		Thread.sleep(3000);
+		LoginPage log = new LoginPage();
+		log.verifyLog();
+
+	}
 
 }
